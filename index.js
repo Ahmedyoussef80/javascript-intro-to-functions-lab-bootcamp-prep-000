ff68edb440bc4efcbf42a71b9a583f3a44891f1f
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-03-28 23:12:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ function logWhisper(string) {
 function sayHiToGrandma(string) { if (string.toLowerCase() === string){
   return "I can't hear you!";}}
   
-  function sayHiToGrandma(string) { if (string.toUpperCase === string){
+  function sayHiToGrandma(string) { if (string.toUpperCase() === string){
   return "YES INDEED!";}}
